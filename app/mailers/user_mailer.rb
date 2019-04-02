a84f33b8d3d5b6aa17c8f9ebf075b07ac30999f8
Ife Odugbesan <ifeodugbesan@yahoo.com>
2019-04-02 18:18:28 +0100

    small fixes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -16,6 +16,6 @@ class UserMailer < ApplicationMailer
 
   def send_email_to_contact(contacts)
     @contacts = contacts
-    mail(to: 'ifeodugbesan@yahoo.com', from: "#{@contacts[:email]}", subject: "Email from #{@contacts[:email]}: #{@contacts[:name]}")
+    mail(to: 'ifeodugbesan@yahoo.com', from: "#{@contacts[:email]}", subject: "#{@contacts[:message].truncate(50)}")
   end
 end
