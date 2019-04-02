11e99ed9717ecde43e431051dbaba470ae414947
Ife Odugbesan <ifeodugbesan@yahoo.com>
2019-04-02 14:09:38 +0100

    small chsngs

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get "/about", to: "pages#about"
   get "/contacts", to: "pages#contacts"
   get "/services", to: "pages#services"
+  get "/products", to: "pages#products"
+  get 'download_pdf', to: "pages#download_pdf"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
