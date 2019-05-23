18479626530cc7a126ec06a5836bbde6b3eb7ae6
Ife Odugbesan <ifeodugbesan@yahoo.com>
2019-05-23 18:17:31 +0100

    fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -26,6 +26,7 @@ class PagesController < ApplicationController
   end
 
   def download_pdf
-    send_file "#{Rails.root}/app/assets/images/catalogo-CC2019.pdf", type: "application/pdf", x_sendfile: true
+    send_file "#{Rails.root}/app/assets/images/#{params[:name]}.pdf", type: "application/pdf", x_sendfile: true
   end
+
 end
